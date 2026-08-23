ab190a738447b479bb4b7df36a02101f8d70dd98
HKdenis <kayanjadenish@gmail.com>
2026-08-23 21:03:37 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- Bb.py
+++ Bb.py
@@ -26,7 +26,7 @@ def login():
         
         /* Centered background container */
         .login-container {
-            max-width: 800px;
+            max-width: 700px;
             margin: 80px auto 20px auto;
             background: #ffffff;
             padding: 40px;
