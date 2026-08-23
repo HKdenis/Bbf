113ef9f1eb361bb8e956981cacffcd594a49ac93
HKdenis <kayanjadenish@gmail.com>
2026-08-23 21:02:46 +0300

    Update Bb.py

--- Bb.py
+++ Bb.py
@@ -26,7 +26,7 @@ def login():
         
         /* Centered background container */
         .login-container {
-            max-width: 600px;
+            max-width: 800px;
             margin: 80px auto 20px auto;
             background: #ffffff;
             padding: 40px;
